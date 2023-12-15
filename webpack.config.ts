437a441653f1cf8a578732cdd0f94625bf407f75
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2023-12-15 21:08:56 +0500

    webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,17 +1,30 @@
+const path = require("path");
+
 const config = {
-    mode: 'production', // "production" | "development" | "none"
-    resolve: {
-      extensions: ['*', '.mjs', '.tsx', '.json']
-    },
-    module: {
-      rules: [
-        {
-          test: /\.mjs$/,
-          include: /node_modules/,
-          type: 'javascript/auto'
-        }
-      ]
-    }
-  }
-  
-  module.exports = config
+  mode: "production",
+  entry: "./src/index.js", // Specify your entry file
+  output: {
+    path: path.resolve(__dirname, "dist"), // Specify the output directory
+    filename: "bundle.js", // Specify the output file
+  },
+  resolve: {
+    extensions: [".mjs", ".tsx", ".json", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto",
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      // Add more rules as needed
+    ],
+  },
+};
+
+module.exports = config;
